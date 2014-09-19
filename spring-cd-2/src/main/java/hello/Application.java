b8183d2114ee2b7128318869d4067b4ba54745ed
Daniel Marell <daniel.marell@cag.se>
2014-09-19 23:04:22 +0200

    spring-cd-2: Funkar

--- spring-cd-2/src/main/java/hello/Application.java
+++ spring-cd-2/src/main/java/hello/Application.java
@@ -1,14 +1,27 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(new ApplicationListener<ApplicationStartedEvent>() {
+            @Override
+            public void onApplicationEvent(ApplicationStartedEvent event) {
+                log.info("Application version: " + BuildInfo.getApplicationVersion());
+            }
+        });
+        app.run(args);
     }
 }
+
