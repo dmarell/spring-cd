2bb6d342c0a8d189b3cd0b6ef8cf2cc75c68801a
Daniel Marell <daniel.marell@cag.se>
2014-10-01 21:55:17 +0200

    spring-cd-3: env detect, env config

--- spring-cd-3/src/main/java/hello/Application.java
+++ spring-cd-3/src/main/java/hello/Application.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
@@ -14,7 +17,12 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
     private static Logger log = LoggerFactory.getLogger(Application.class);
 
-    @Autowired private static Config config;
+    @Autowired private Config config;
+
+    @Bean
+    public EmbeddedServletContainerFactory containerFactory() {
+        return new TomcatEmbeddedServletContainerFactory(config.getHttpPort());
+    }
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
